97a9e9a2ec870c83f02d32f484dd761b41f947c1
Mats Andreassen <matsand@live.no>
2020-05-05 00:14:48 +0200

    Added correlationId

--- QueueSMS/index.js
+++ QueueSMS/index.js
@@ -1,38 +1,32 @@
+const { v4: uuid } = require('uuid');
+const { acceptedRes, errorRes } = require('../lib/http-result')
 const validateJson = require('../lib/validate-sms-json')
 
 module.exports = async function (context, request) {
-  const data = request.body
+  const correlationId = request.headers['correlationId'] || context.invocationId || uuid()
+
+  const data = {
+    correlationId,
+    submittedAt: new Date().toISOString(),
+    ...request.body
+  }
 
   // Verify that input data is present and correct
   const validation = validateJson(data)
   if (validation && validation.errors) {
-    context.res = {
-      status: 400,
-      body: {
-        message: 'One or more field has invalid data, please see usage here: https://github.com/vtfk/azf-send-sms',
-        errors: validation.errors
-      }
-    }
-    return
+    return context.res = errorRes('One or more field has invalid data, please see usage here: https://github.com/vtfk/azf-send-sms!', validation.errors, correlationId, 400) 
   }
 
   try {
+    // Push the data into the queue
     context.bindings.queue = data
-    context.log('info', ['index', 'send', 'receivers', data.receivers, 'queued'])
+    context.log('info', ['index', 'queue', correlationId, 'receivers', data.receivers, 'queued'])
 
-    context.res = {
-      status: 202,
-      body: 'Queued!',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    }
+    // Build status URL and return 202
+    const statusEndpoint = `https://${process.env.WEBSITE_HOSTNAME}/api/sms/${correlationId}`;
+    context.res = acceptedRes(statusEndpoint, `SMS added to Queue! Status: ${statusEndpoint}`, correlationId)
   } catch (error) {
-    context.log.error('error', ['index', 'send', 'receivers', data.receivers, error])
-
-    context.res = {
-      status: 500,
-      body: 'Something happened! ' + error.message
-    }
+    context.log.error('error', ['index', 'queue', correlationId, 'receivers', data.receivers, error])
+    context.res = errorRes('Something happened!', error.message, correlationId)
   }
 }
