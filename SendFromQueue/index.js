13cf3e1b20c80f219a04375eee485fd22559f3d2
Mats Andreassen <matsand@live.no>
2020-05-05 00:16:05 +0200

    Create blob file with API result

--- SendFromQueue/index.js
+++ SendFromQueue/index.js
@@ -1,27 +1,39 @@
+const { v4: uuid } = require('uuid')
 const validateJson = require('../lib/validate-sms-json')
 const sendSms = require('../lib/send-sms')
 
 module.exports = async function (context, sms) {
+  const correlationId = sms.correlationId || uuid()
+
   // Validate JSON just to be sure that everything is OK..
   const validation = validateJson(sms)
   if (validation && validation.errors) {
-    return context.done(validation.errors)
+    return context.done({ ...validation.errors, correlationId })
   }
 
   try {
     // Send SMS to PSWinCom
-    context.log('info', ['index', 'send', 'receivers', sms.receivers, 'sending sms'])
+    context.log('info', ['index', 'send', correlationId, 'receivers', sms.receivers, 'sending sms'])
     const result = await sendSms(context, sms)
+
+    context.log('info', ['index', 'send', correlationId, 'receivers', sms.receivers, 'sms sent', result])
 
     // Validate result from PsWinCom
     if (result.login === 'FAIL' || (result.refs && Object.keys(result.refs).length < 1)) {
       throw result
     }
 
-    context.log('info', ['index', 'send', 'receivers', sms.receivers, 'sms sent', result])
-    context.done(null, result)
+    // Export file to blob storage
+    context.bindings.storage = {
+      correlationId,
+      submittedAt: sms.submittedAt,
+      completedAt: new Date().toISOString(),
+      result
+    }
+
+    context.log('info', ['index', 'send', correlationId, 'blob set'])
   } catch (error) {
-    context.log.error('error', ['index', 'send', 'receivers', sms.receivers, error])
+    context.log.error('error', ['index', 'send', correlationId, 'receivers', sms.receivers, error])
     context.done(error)
   }
 }
