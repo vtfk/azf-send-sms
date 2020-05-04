efeb8f446c68805d7bf8b7bdf50171982aac57e8
Mats Andreassen <matsand@live.no>
2020-05-05 00:15:03 +0200

    Standard

--- QueueSMS/index.js
+++ QueueSMS/index.js
@@ -1,9 +1,9 @@
-const { v4: uuid } = require('uuid');
+const { v4: uuid } = require('uuid')
 const { acceptedRes, errorRes } = require('../lib/http-result')
 const validateJson = require('../lib/validate-sms-json')
 
 module.exports = async function (context, request) {
-  const correlationId = request.headers['correlationId'] || context.invocationId || uuid()
+  const correlationId = request.headers.correlationId || context.invocationId || uuid()
 
   const data = {
     correlationId,
@@ -14,7 +14,8 @@ module.exports = async function (context, request) {
   // Verify that input data is present and correct
   const validation = validateJson(data)
   if (validation && validation.errors) {
-    return context.res = errorRes('One or more field has invalid data, please see usage here: https://github.com/vtfk/azf-send-sms!', validation.errors, correlationId, 400) 
+    context.res = errorRes('One or more field has invalid data, please see usage here: https://github.com/vtfk/azf-send-sms!', validation.errors, correlationId, 400)
+    return
   }
 
   try {
@@ -23,7 +24,7 @@ module.exports = async function (context, request) {
     context.log('info', ['index', 'queue', correlationId, 'receivers', data.receivers, 'queued'])
 
     // Build status URL and return 202
-    const statusEndpoint = `https://${process.env.WEBSITE_HOSTNAME}/api/sms/${correlationId}`;
+    const statusEndpoint = `https://${process.env.WEBSITE_HOSTNAME}/api/sms/${correlationId}`
     context.res = acceptedRes(statusEndpoint, `SMS added to Queue! Status: ${statusEndpoint}`, correlationId)
   } catch (error) {
     context.log.error('error', ['index', 'queue', correlationId, 'receivers', data.receivers, error])
