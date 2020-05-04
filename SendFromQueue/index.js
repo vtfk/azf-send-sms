9d647e5d81992a3f689ea63ea5f04a32452ca0b7
Mats Andreassen <matsand@live.no>
2020-05-04 19:26:16 +0200

    Send SMS when added to queue

--- SendFromQueue/index.js
+++ SendFromQueue/index.js
@@ -1,3 +1,27 @@
-module.exports = async function (context, myQueueItem) {
-  context.log('JavaScript queue trigger function processed work item', myQueueItem)
+const validateJson = require('../lib/validate-sms-json')
+const sendSms = require('../lib/send-sms')
+
+module.exports = async function (context, sms) {
+  // Validate JSON just to be sure that everything is OK..
+  const validation = validateJson(sms)
+  if (validation && validation.errors) {
+    return context.done(validation.errors)
+  }
+
+  try {
+    // Send SMS to PSWinCom
+    context.log('info', ['index', 'send', 'receivers', sms.receivers, 'sending message'])
+    const result = await sendSms(context, sms)
+
+    // Validate result from PsWinCom
+    if (result.login === 'FAIL' || (result.refs && Object.keys(result.refs).length < 1)) {
+      throw result
+    }
+
+    context.log('info', ['index', 'send', 'receivers', sms.receivers, 'message sent', result])
+    context.done(null, result)
+  } catch (error) {
+    context.log.error('error', ['index', 'send', 'receivers', sms.receivers, error])
+    context.done(error)
+  }
 }
