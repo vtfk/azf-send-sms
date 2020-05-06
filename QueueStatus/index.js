dd0c0a3fe7424c39bb5adec850303aa4d901110f
Mats Andreassen <matsand@live.no>
2020-05-07 00:21:08 +0200

    Allow different HTTP status code on method

--- QueueStatus/index.js
+++ QueueStatus/index.js
@@ -12,9 +12,11 @@ module.exports = async function (context, request) {
   const blobData = context.bindings.storage
 
   if (blobData) {
-    // Return returned blob data if set
-    context.res = okRes(blobData, correlationId)
-    context.log('info', ['status', correlationId, 'OK'])
+    // Return returned blob data if set, if there is a result, return 200.
+    const httpStatus = blobData.result ? 200 : 500
+
+    context.res = okRes(blobData, correlationId, httpStatus)
+    context.log('info', ['status', correlationId, httpStatus])
   } else {
     // Check back in 10 seconds!
     context.res = acceptedRes(request.originalUrl, 'Not finished yet, check back in a bit..', correlationId)
