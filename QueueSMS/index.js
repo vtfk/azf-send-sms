16397f9e0257a53cc442a20ee4421837f9a34126
Mats Andreassen <matsand@live.no>
2020-05-06 11:07:30 +0200

    Initial wait should be the average backend execution time

--- QueueSMS/index.js
+++ QueueSMS/index.js
@@ -25,7 +25,7 @@ module.exports = async function (context, request) {
 
     // Build status URL and return 202
     const statusEndpoint = `https://${process.env.WEBSITE_HOSTNAME}/api/sms/${correlationId}`
-    context.res = acceptedRes(statusEndpoint, `SMS added to Queue! Status: ${statusEndpoint}`, correlationId)
+    context.res = acceptedRes(statusEndpoint, `SMS added to Queue! Status: ${statusEndpoint}`, correlationId, 1)
   } catch (error) {
     context.log.error('error', ['queue', correlationId, 'receivers', data.receivers, error])
     context.res = errorRes('Something happened!', error.message, correlationId)
